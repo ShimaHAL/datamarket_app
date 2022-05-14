47277f86dcf57cd074bc19345327f1928b840a09
ShimaHAL <80742820+ShimaHAL@users.noreply.github.com>
2022-05-14 18:22:57 +0900

    一応完成

--- dmapp/views.py
+++ dmapp/views.py
@@ -1,4 +1,5 @@
-from typing import List, Dict, Any
+from multiprocessing import Value
+from typing import List, Dict, Any, Optional
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.decorators.http import require_POST
@@ -15,63 +16,66 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "dmapp/index.html")
 
 
-@require_POST
 def search(request: HttpRequest) -> HttpResponse:
 
-    search: str = str(request.POST["search"])
+    context: Dict[str, bool | str] = {}
+    search_word: str = request.GET.get("search", "")
 
-    if not search:
-        return redirect(reverse("index"))
-
-    else:
-        token_filters = [
-            POSKeepFilter(["名詞", "動詞", "形容詞"]),
-            LowerCaseFilter(),
-            ExtractAttributeFilter("base_form"),
-        ]
-        analyzer = Analyzer(token_filters=token_filters)
-
-        wakati = list(analyzer.analyze(search))
-        url: str = "https://datajacket-store.org/sparql"
-        query: str = "select ?dj_ID ?DJ ?sol ?req where{\
-            {?s rdfs:label ?sol;\
-            <http://datajacket.org/solution/id> ?sol_ID;\
-            <http://datajacket.org/solution/combine> ?dj_URI;\
-            <http://datajacket.org/solution/satisfy> ?req_URI.\
-            ?req_URI rdfs:label ?req;\
-            <http://datajacket.org/requirement/id> ?req_ID.\
-            ?dj_URI rdfs:label ?DJ;\
-            <http://datajacket.org/datajacket/id> ?dj_ID."
-
-        query += (
-            "FILTER ("
-            + " || ".join(['regex(?req, "' + w + '")' for w in wakati])
-            + ")"
-        )
-        query += "} UNION {"
-        query += (
-            "FILTER ("
-            + " || ".join(['regex(?sol, "' + w + '")' for w in wakati])
-            + ")"
-        )
-        query += "} UNION {"
-        query += "?s rdfs:label ?DJ; <http://datajacket.org/datajacket/id> ?dj_ID."
-        query += (
-            "FILTER ("
-            + " || ".join(['regex(?DJ, "' + w + '")' for w in wakati])
-            + ")"
-        )
-        query += "}}"
-        query = "?query=" + urllib.parse.quote(query)
-
-        params: str = "&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+"
-        print(url + query + params)
-        res = requests.get(url + query + params)
-        print(wakati)
-        context: Dict[str, Any] = {"message": "search", "wakati": wakati}
+    if not search_word:
+        context["status"] = False
         return JsonResponse(context)
 
+    token_filters = [
+        POSKeepFilter(["名詞", "動詞", "形容詞"]),
+        LowerCaseFilter(),
+        ExtractAttributeFilter("base_form"),
+    ]
+    analyzer = Analyzer(token_filters=token_filters)
 
-@require_POST
-def feellucky(request: HttpRequest) -> HttpResponse:
-    return JsonResponse({"message": "feel lucky"})
+    wakati = list(analyzer.analyze(search_word))
+    url: str = "https://datajacket-store.org/sparql"
+
+    query: str = ""
+    mode: str = request.GET.get("mode", "dj")
+    if mode == "DJ":
+        query += "select ?id ?DJ where{\
+            ?s <http://datajacket.org/datajacket/id> ?id;\
+            rdfs:label ?DJ."
+
+        query += (
+            "FILTER (" + " || ".join(['regex(?DJ, "' + w + '")' for w in wakati]) + ")"
+        )
+        query += "}"
+    elif mode == "req":
+        query += "select ?id ?req where{\
+            ?s <http://datajacket.org/solution/id> ?id;\
+            rdfs:label ?req."
+        query += (
+            "FILTER (" + " || ".join(['regex(?req, "' + w + '")' for w in wakati]) + ")"
+        )
+        query += "}"
+
+    else:  # mode=="sol"
+        query += "select ?id ?sol where{\
+            ?s <http://datajacket.org/requirement/id> ?id;\
+            rdfs:label ?sol."
+        query += (
+            "FILTER (" + " || ".join(['regex(?sol, "' + w + '")' for w in wakati]) + ")"
+        )
+        query += "}"
+    query = "?query=" + urllib.parse.quote(query)
+    params: str = (
+        "&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on&run=+Run+Query+"
+    )
+    res = requests.get(url + query + params)
+    jsonList: List = res.json()["results"]["bindings"]
+    context["status"] = True
+
+    html: str = ""
+    for data in jsonList:
+        value: str = data[mode]["value"]
+        html += "<tr>"
+        html += f"<td data-value='{value}' onclick='searchAgain(this);'>{value}</td>"
+        html += "</tr>"
+    context["html"] = html
+    return JsonResponse(context)
